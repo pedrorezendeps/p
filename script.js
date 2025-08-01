5216193d31911ce2be1ee65ef07ef537aaa8e0f3
Pedro Rezende <pedrojpd3@gmail.com>
2025-08-01 14:10:57 -0300

    vesao final

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ function togglemode(){
     const img = document.querySelector("#profile img")
 
     if (html.classList.contains("light")){
-        img.setAttribute("src",   "./assets/avatar-light.png")
+        img.setAttribute("src",   "./assets/lightttt.png")
     } else{
-        img.setAttribute("src",   "./assets/avatar.png")
+        img.setAttribute("src",   "./assets/dsdads.png")
     }
 
 
 
 }
+
+const imagemPerfil = document.querySelector("#profile img");
+
+imagemPerfil.addEventListener("click", () => {
+  imagemPerfil.classList.add("animar");
+
+  setTimeout(() => {
+    imagemPerfil.classList.remove("animar");
+  }, 400);
+});
